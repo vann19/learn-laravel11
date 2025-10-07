209e844db7577ddca33b072eb2d377908e3b6748
vann19 <ahmadfansya660@gmail.com>
2025-10-07 13:15:24 +0700

    middleware - melakukan aksi sebelum atau sesudah request dilanjutkan

--- app/Http/Middleware/CheckMembership.php
+++ app/Http/Middleware/CheckMembership.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckMembership
@@ -18,6 +19,21 @@ class CheckMembership
         if(!$request->membership == true) {
             return redirect('/pricing');
         }
-        return $next($request);
+
+        Log::info('Before Request:' , [
+            'url' => $request->url(),
+            'params' => $request->all(),
+        ]);
+
+        $response = $next($request);
+
+        sleep(2);
+
+        Log::info('After Request:' , [
+            'status' => $response->getStatusCode(),
+            'content' => $response->getContent(),
+        ]);
+        
+        return $response;
     }
 }
